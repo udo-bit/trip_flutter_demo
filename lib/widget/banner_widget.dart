048764aa5a6a47d3829e22b029b389d991eda15c
udo-bit <enote_gxl@163.com>
2024-12-03 15:42:23 +0800

    slide

--- lib/widget/banner_widget.dart
+++ lib/widget/banner_widget.dart
@@ -8,7 +8,6 @@ class BannerWidget extends StatefulWidget {
     this.bannerList, {
     super.key,
   });
-
   @override
   State<BannerWidget> createState() => _BannerWidgetState();
 }
@@ -28,12 +27,15 @@ class _BannerWidgetState extends State<BannerWidget> {
                 .toList(),
             options: CarouselOptions(
               onPageChanged: (index, reason) {
-                _current = index;
+                setState(() {
+                  _current = index;
+                });
               },
               height: 200.px,
               autoPlay: true,
               viewportFraction: 1.0,
-            ))
+            )),
+        Positioned(left: 0, right: 0, bottom: 10, child: _indicator())
       ],
     );
   }
@@ -46,6 +48,28 @@ class _BannerWidgetState extends State<BannerWidget> {
         width: width,
         fit: BoxFit.cover,
       ),
+    );
+  }
+
+  Widget _indicator() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: widget.bannerList.asMap().entries.map((entry) {
+        return GestureDetector(
+          onTap: () {
+            _controller.animateToPage(entry.key);
+          },
+          child: Container(
+            margin: const EdgeInsets.symmetric(horizontal: 5, vertical: 10),
+            width: 10,
+            height: 10,
+            decoration: BoxDecoration(
+              shape: BoxShape.circle,
+              color: _current == entry.key ? Colors.blue : Colors.grey,
+            ),
+          ),
+        );
+      }).toList(),
     );
   }
 }
